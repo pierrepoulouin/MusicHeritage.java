67d833f4311d4b3ca1ed6ab05dd042526aef66be
DJ Logan <pierrepoulouin@gmail.com>
2018-07-30 12:20:45 +0200

    First Commit : Create Music Class

---
+++ MusicHeritage/src/musicHeritage/Music.java
@@ -0,0 +1,119 @@
+package musicHeritage;
+
+
+/**
+ * 
+ */
+public class Music  {
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDrum() {
+		return drum;
+	}
+
+	public void setDrum(String drum) {
+		this.drum = drum;
+	}
+
+	public String getStyle() {
+		return style;
+	}
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
+
+	public String getGuitar() {
+		return guitar;
+	}
+
+	public void setGuitar(String guitar) {
+		this.guitar = guitar;
+	}
+
+	public String getSing() {
+		return sing;
+	}
+
+	public void setSing(String sing) {
+		this.sing = sing;
+	}
+
+	public String getBass() {
+		return bass;
+	}
+
+	public void setBass(String bass) {
+		this.bass = bass;
+	}
+
+	public int getYearOfCreation() {
+		return yearOfCreation;
+	}
+
+	public void setYearOfCreation(int yearOfCreation) {
+		this.yearOfCreation = yearOfCreation;
+	}
+
+	public int getMembers() {
+		return members;
+	}
+
+	public void setMembers(int members) {
+		this.members = members;
+	}
+
+	/**
+	 * Default constructor
+	 */
+	public Music() {
+	}
+
+	/**
+	 * 
+	 */
+	public String name;
+
+	/**
+	 * 
+	 */
+	public String drum;
+
+	/**
+	 * 
+	 */
+	public String style;
+
+	/**
+	 * 
+	 */
+	public String guitar;
+
+	/**
+	 * 
+	 */
+	public String sing;
+
+	/**
+	 * 
+	 */
+	public String bass;
+
+	/**
+	 * 
+	 */
+	public int yearOfCreation;
+
+	/**
+	 * 
+	 */
+	public int members;
+
+}
